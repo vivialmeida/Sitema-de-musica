d7dd735efcfe972be11e2f7b4f4a1192faf28d9e
viviane.almeida <viviane.almeida@armateus.com.b>
2020-01-15 08:49:08 -0300

    Removendo comentarios

--- app/infra/ConnectionFactory.js
+++ app/infra/ConnectionFactory.js
@@ -3,13 +3,11 @@ var config = {
     user: 'postgres',
     database: 'postgres',
     password: '123456',
-    // schemas: 'emusic',
     port: 5432
 };
 
 const pool = null;      
 
-// solução 03: Usando classes
 class ConnectionFactory {
 
     constructor() {
@@ -19,18 +17,6 @@ class ConnectionFactory {
 
 }
 
-    
-    // 
-    //     this._conexao = ps.createConnection({
-          
-    //         host: 'localhost',
-    //         user: 'postgres',
-    //         password: '123456',
-    //         database: 'emusic'
-    //     });
-    // }
-
-
     getConexao() {
         return this._conexao;
     }
@@ -39,39 +25,3 @@ class ConnectionFactory {
 
 module.exports = () => { return ConnectionFactory };
 
-
-
-// solução 02: Para evitar fazer a conexão com o consign
-/* 
-
-const criaConexao =  function() {
-    console.log('conexão estabelecida com o banco de dados');
-    return ps.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'lojajs'
-    });
-}
-
-module.exports = function() {
-    console.log('carregando o modulo de conexao');
-    return criaConexao;
-}
-
- */
-
-/* solucao 01 */
-
-/* 
-const ps = require('ps');
-
-module.exports = function() {
-    console.log('conexão estabelecida com o banco de dados');
-    return ps.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'lojajs'
-    });
-} */
